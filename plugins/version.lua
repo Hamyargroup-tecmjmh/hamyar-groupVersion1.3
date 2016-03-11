67564c0e3c81339b3f7c25dda3740d93d58c6574
mhmjhamyargroup44233 <mhmjhamyargroup44233@users.noreply.github.com>
2016-03-12 02:03:48 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,17 +1,19 @@
 do
 
 function run(msg, matches)
-  return 'NOD32-BOT v8'.. VERSION .. [[ 
- 
-  Developer : @Behroozyaghi
-  
-  Channel : @Nod32team
-  
-  Open git : https://github.com/BH-YAGHI/NOD32-BOT.git
-  
-  NOD32 VERSION 8
-  
-  All rights reserved.
+  return 'HamyarGroup v1.3'.. VERSION .. [[
+
+🔧توسعه دهنده ربات:@mhmafi~~@Ho3yyyn 
+
+🎁کانال ربات: @hamyargroup
+
+📰سایت رسمی ربات:https://hg.hamyartec.ir
+
+امکانات این نسخه:
+✅سرعت بالا در پاسخ 
+✅آپشن های سرگرم کننده فراوان 
+
+↙️برای مشاهده آپشن های بیشتر و دقیقتر شما میتوانید به سایت رسمی مراجعه نمایید. 
   __________________]]
 end
 
